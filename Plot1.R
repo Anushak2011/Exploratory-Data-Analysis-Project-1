c422116687f7ef491a209b7778782002fcd2ab9f
Anushak2011 <anushak@ucla.edu>
2015-09-12 19:41:59 -0700

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -3,7 +3,8 @@ source("load_data.R")
 plot1 <- function(data=NULL) {
     if(is.null(data))
         data <- load_data()
-    
+      power <- read.table(file, header=T, sep=";")
+    power$Date <- as.Date(power$Date, format="%d/%m/%Y")
     png("plot1.png", width=400, height=400)
     
     hist(data$Global_active_power,
